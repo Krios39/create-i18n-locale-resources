81abcc3ebab39a72b17bf15aa7f934b605ebe7c5
Krios39 <neshchadimov.ruslan@gmail.com>
2024-09-12 16:59:56 +0500

    add ability to not create ns types

--- index.js
+++ index.js
@@ -2,17 +2,27 @@
 
 I18nManager = require('./i18n-manager');
 
+const FLAGS = {
+    notCreateTranslationNSTypes: "-not-types"
+}
+
 const args = process.argv.slice(2);
 
-const pathArgKey =  '--path'
+const pathArgKey = '--path'
 const pathIndex = args.indexOf(pathArgKey)
 
-
 const defaultBasePath = './example'
+const path = args[pathIndex + 1]
 
-const basePath = pathIndex !== -1 && args.length > pathIndex ? args[pathIndex+1] : defaultBasePath;
+if (pathIndex !== -1 && path === undefined) {
+    throw new Error('Not Found Path!')
+}
+
+const basePath = pathIndex !== -1 && args.length > pathIndex ? args[pathIndex + 1] : defaultBasePath;
+
+const createTranslationNSTypes = !args.some(arg => arg === FLAGS.notCreateTranslationNSTypes);
 
 const i18nManager = new I18nManager(basePath);
-i18nManager.generateNSContent();
+i18nManager.generateNSContent(createTranslationNSTypes);
 i18nManager.generateLanguageFiles();
 
